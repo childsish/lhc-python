431fe0cba8da80a516f762148012ae4ccaf9fa5e
Liam Childs <l.childs@dkfz-heidelberg.de>
2016-10-13 18:00:40 +0200

    added repr to genomic position

--- lhc/binf/genomic_coordinate/genomic_position.py
+++ lhc/binf/genomic_coordinate/genomic_position.py
@@ -8,6 +8,9 @@ class GenomicPosition(object):
     def __str__(self):
         return '{}:{}'.format(self.chromosome, self.position + 1)
 
+    def __repr__(self):
+        return 'GenomicPosition({})'.format(self)
+
     def __eq__(self, other):
         return self.chromosome == other.chromosome and self.position == other.position and\
             self.strand == other.strand
@@ -42,5 +45,5 @@ class GenomicPosition(object):
         :rtype: int
         """
         if self.chromosome != other.chromosome:
-            raise ValueError('Positions not on same chromosome')
+            raise ValueError('Positions on different chromosomes: "{}" and "{}"'.format(self.chromosome, other.chromosome))
         return self.position - other.position
