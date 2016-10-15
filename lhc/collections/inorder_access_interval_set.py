6ebe4ae513ef26a5cca7729bb5c5b0779dc3e248
Liam Childs <liam_childs@hotmail.com>
2016-10-15 23:29:58 +0200

    updated inorder access sets

--- lhc/collections/inorder_access_interval_set.py
+++ lhc/collections/inorder_access_interval_set.py
@@ -1,49 +1,30 @@
-from bisect import bisect_right
-from itertools import chain
+import bisect
 
 
 class InOrderAccessIntervalSet(object):
-    def __init__(self, iterator, set_key=None, query_key=None):
-        self.starts = []
-        self.stops = []
-        self.buffer = []
+    def __init__(self, iterator):
+        self._iterator = iterator
+        self._stops = []
+        self._items = []
+        self._item = next(self._iterator)
 
-        self.iterator = iterator
-        self.set_key_fn = default_key_function if set_key is None else set_key
-        self.query_key_fn = default_key_function if query_key is None else query_key
+    def __getitem__(self, key):
+        stops = self._stops
+        items = self._items
 
-    def fetch(self, *args):
-        """
-        Fetch the items within the given interval. The start of the given interval must be greater than or equal to the
-        start of the previously used interval. The number of arguments must be equal to the number of dimensions + 1.
-        The second last argument is the start position in the last dimension and the last argument is the stop position
-        in the last dimension.
+        while len(stops) > 0 and stops[0] <= key.start:
+            stops.pop(0)
+            items.pop(0)
 
-        :param args: interval to retrieve
-        :return: list of items from the iterator
-        """
-        start = self.query_key_fn(args[0] if len(args) == 2 else args[:-1])
-        stop = self.query_key_fn(args[1] if len(args) == 2 else (args[:-2] + args[-1:]))
+        item = self._item
+        try:
+            while item.start < key.stop and key.start < item.stop:
+                index = bisect.bisect_right(stops, item.stop)
+                stops.insert(index, item.stop)
+                items.insert(index, item)
+                item = next(self._iterator)
+        except StopIteration:
+            pass
+        self._item = item
 
-        for item in self.iterator:
-            key = self.set_key_fn(item)
-            if key.start >= stop:
-                self.iterator = chain([item], self.iterator)
-                break
-            index = bisect_right(self.stops, key.stop)
-            self.starts.insert(index, key.start)
-            self.stops.insert(index, key.stop)
-            self.buffer.insert(index, item)
-
-        cut_index = 0
-        while cut_index < len(self.stops) and self.stops[cut_index] <= start:
-            cut_index += 1
-        self.starts = self.starts[cut_index:]
-        self.stops = self.stops[cut_index:]
-        self.buffer = self.buffer[cut_index:]
-
-        return sorted(item for item_start, item in zip(self.starts, self.buffer) if item_start < stop)
-
-
-def default_key_function(x):
-    return x
+        return sorted(items)
--- lhc/collections/inorder_access_set.py
+++ lhc/collections/inorder_access_set.py
@@ -1,39 +1,26 @@
-from itertools import chain
+from collections import deque
+from itertools import islice
 
 
 class InOrderAccessSet(object):
-    def __init__(self, iterator, key=None):
-        self.key_fn = (lambda x: x) if key is None else key
+    def __init__(self, iterator):
+        self._iterator = iterator
+        self._items = deque()
 
-        self.keys = []
-        self.buffer = []
-        self.iterator = iterator
+    def __getitem__(self, key):
+        items = self._items
 
-    def fetch(self, *args):
-        """
-        Fetch the items within the given interval. The start of the given interval must be greater than or equal to the
-        start of the previously used interval. The number of arguments must be equal to the number of dimensions + 1.
-        The second last argument is the start position in the last dimension and the last argument is the stop position
-        in the last dimension.
+        while len(items) > 0 and items[0] < key.start:
+            items.popleft()
 
-        :param args: interval to retrieve
-        :return: list of items from the iterator
-        """
-        start = self.key_fn(args[0] if len(args) == 2 else args[:-1])
-        stop = self.key_fn(args[1] if len(args) == 2 else (args[:-2] + args[-1:]))
+        try:
+            while len(items) == 0 or items[-1] < key.stop:
+                item = next(self._iterator)
+                if key.start <= item:
+                    items.append(item)
+        except StopIteration:
+            pass
 
-        for item in self.iterator:
-            key = self.key_fn(item)
-            if key >= stop:
-                self.iterator = chain([item], self.iterator)
-                break
-            self.keys.append(key)
-            self.buffer.append(item)
-
-        cut_index = 0
-        while cut_index < len(self.keys) and self.keys[cut_index] < start:
-            cut_index += 1
-        self.keys = self.keys[cut_index:]
-        self.buffer = self.buffer[cut_index:]
-
-        return sorted(self.buffer)
+        if len(items) > 0 and items[-1] >= key.stop:
+            return list(islice(items, 0, len(items) - 1))
+        return list(items)
