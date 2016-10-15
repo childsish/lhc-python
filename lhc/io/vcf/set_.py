b7d3ae08b99eceda27f17cb5313bf06a8bcf9fef
Liam Childs <liam_childs@hotmail.com>
2016-10-15 23:29:47 +0200

    fixed vcf set

--- lhc/io/vcf/set_.py
+++ lhc/io/vcf/set_.py
@@ -7,8 +7,8 @@ class VcfSet(object):
         self.data = list(iterator)
         self.ivl_index = MultiDimensionMap([str, Interval])
         for i, variant in enumerate(self.data):
-            ivl = Interval(variant.pos.position, variant.pos.position + len(variant.ref))
-            self.ivl_index[(variant.pos.chromosome, ivl)] = i
+            ivl = Interval(variant.position, variant.position + len(variant.data['ref']))
+            self.ivl_index[(variant.chromosome, ivl)] = i
 
     def fetch(self, chr, start, stop=None):
         if stop is None:
