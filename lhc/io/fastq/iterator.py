1b1e6d9e40dcb940a14d22a39691c9cdb39428cc
Liam Childs <liam.childs@pei.de>
2019-08-20 13:54:59 +0200

    #240 simplify fastq iterator

--- lhc/io/fastq/iterator.py
+++ lhc/io/fastq/iterator.py
@@ -1,27 +1,12 @@
 from collections import namedtuple
-from lhc.itertools.chunked_iterator import ChunkedIterator
+from itertools import islice
 
 
-class FastqEntry(namedtuple('FastqEntry', ('hdr', 'seq', 'qual_hdr', 'qual'))):
+class FastqEntry(namedtuple('FastqEntry', ('hdr', 'seq', 'qual'))):
     def __str__(self):
         return '@{}\n{}\n{}+\n{}\n'.format(self.hdr, self.seq, self.qual_hdr, self.qual)
 
 
-class FastqEntryIterator(object):
-    def __init__(self, iterator):
-        self.iterator = iterator
-        self.it = ChunkedIterator(self.iterator, 4)
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        seq_id, seq, qual_id, qual = next(self.it)
-        return FastqEntry(seq_id.strip()[1:],
-                          seq.strip(),
-                          qual_id.strip()[1:],
-                          qual.strip())
-
-    def __del__(self):
-        if hasattr(self.iterator, 'close'):
-            self.iterator.close()
+def iter_fastq(iterator):
+    for hdr, seq, qual_hdr, qual in islice(iterator, start=0, stop=None, step=4):
+        yield FastqEntry(hdr.strip()[1:], seq.strip(), qual.strip())
