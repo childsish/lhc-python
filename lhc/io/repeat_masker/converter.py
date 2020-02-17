aa82a526cefe63790d39e6c70309886abf16e11b
Liam Childs <liam.childs@pei.de>
2020-02-17 09:46:11 +0100

    fix repeat masker parsing

--- lhc/io/repeat_masker/converter.py
+++ lhc/io/repeat_masker/converter.py
@@ -11,7 +11,7 @@ class RepeatMaskerConverter(GenomicIntervalConverter):
 
     def __init__(self, lines: Iterator[str]):
         self.lines = lines
-        self.gene_ids = Counter()
+        self.transcript_ids = Counter()
         self.unknown_genes = 0
 
     def __iter__(self) -> Iterable[GenomicInterval]:
@@ -29,8 +29,10 @@ class RepeatMaskerConverter(GenomicIntervalConverter):
         except IndexError:
             self.unknown_genes += 1
             gene_id = 'unknown_{}'.format(self.unknown_genes)
-        self.gene_ids[gene_id] += 1
-        transcript_id = '{}.{}'.format(gene_id, self.gene_ids[gene_id])
+        transcript_id = '{}.1'.format(gene_id)
+        self.transcript_ids[transcript_id] += 1
+        exon_number = self.transcript_ids[transcript_id]
+        exon_id = '{}.{}'.format(transcript_id, exon_number)
         return GenomicInterval(
             int(parts[5]) - 1,
             int(parts[6]),
@@ -38,7 +40,8 @@ class RepeatMaskerConverter(GenomicIntervalConverter):
             strand='+' if parts[8] == '+' else '-',
             data={'score': parts[0], 'divergence': parts[1], 'deletion': parts[2], 'insertion': parts[3],
                   'subfamily_id': parts[9], 'class_id': class_, 'family_id': ''.join(family), 'gene_id': gene_id,
-                  'transcript_id': transcript_id, 'source': 'RepeatMasker', 'feature': 'repeat_element', 'frame': '0'})
+                  'transcript_id': transcript_id, 'source': 'RepeatMasker', 'feature': 'repeat_element', 'frame': '0',
+                  'exon_id': exon_id, 'exon_number': exon_number})
 
     def format(self, interval: GenomicInterval):
         raise NotImplementedError('This function has not yet been implemented.')
