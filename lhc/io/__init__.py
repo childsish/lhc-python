f82af313ca1b30fc5812454187e013178db0a7af
Liam Childs <liam.childs@pei.de>
2020-02-18 09:34:27 +0100

    removed open_file conflict

--- lhc/io/__init__.py
+++ lhc/io/__init__.py
@@ -1,1 +1,6 @@
-from .open_file import open_file
+from .file import open_file
+
+# loci formats
+from .gff import iter_gff, format_gff
+from .gtf import iter_gtf, format_gtf
+from .region import iter_region, format_region
