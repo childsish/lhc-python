ec872988173063545ed9cc356c2f0ba4a2080359
Liam Childs <liam.childs@pei.de>
2020-02-21 16:59:10 +0100

    update merge_sorted to return entire top if equal

--- lhc/itertools/merge_sorted.py
+++ lhc/itertools/merge_sorted.py
@@ -1,7 +1,7 @@
-from typing import Any, Iterator, List
+from typing import Any, Iterator, List, Tuple
 
 
-def merge_sorted(*iterators: List[Any]) -> Iterator[List[Any]]:
+def merge_sorted(*iterators: Iterator[Any]) -> Iterator[List[List[Any]]]:
     tops = [next(iterator, None) for iterator in iterators]
     smallest_indices = get_smallest_indices(tops)
     while len(smallest_indices) > 0:
@@ -9,7 +9,7 @@ def merge_sorted(*iterators: List[Any]) -> Iterator[List[Any]]:
         smallest_indices = get_smallest_indices(tops)
 
 
-def get_smallest_indices(tops):
+def get_smallest_indices(tops) -> List[int]:
     smallest = []
     for i, item in enumerate(tops):
         if item is None:
@@ -22,8 +22,12 @@ def get_smallest_indices(tops):
     return smallest
 
 
-def get_at_indices(tops, indices: List[int], iterators: List[Iterator]) -> List[Any]:
-    result = [tops[index] for index in indices]
+def get_at_indices(tops, indices: List[int], iterators: Tuple[Iterator[Any]]) -> List[List[Any]]:
+    result = []
     for index in indices:
+        result.append([tops[index]])
         tops[index] = next(iterators[index], None)
+        while tops[index] == result[-1][0]:
+            result[-1].append(tops[index])
+            tops[index] = next(iterators[index], None)
     return result
