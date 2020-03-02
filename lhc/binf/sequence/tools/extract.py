4410fe9ebc71c25531432cbf11591e225accd74b
Liam Childs <liam.childs@pei.de>
2020-03-02 10:28:31 +0100

    add extract by id to loci extract

--- lhc/binf/sequence/tools/extract.py
+++ lhc/binf/sequence/tools/extract.py
@@ -2,22 +2,35 @@ import argparse
 import pysam
 
 from textwrap import TextWrapper
-from typing import Generator, Iterable, Set
+from typing import Generator, Iterable, Optional, Set
 from lhc.binf.genomic_coordinate import GenomicInterval
 from lhc.binf.sequence.reverse_complement import reverse_complement
 from lhc.io.loci import open_loci_file
 from lhc.io.file import open_file
 
 
-def extract(regions: Iterable[GenomicInterval], sequences: pysam.FastaFile) -> Generator[str, None, Set[str]]:
+def extract(regions: Iterable[GenomicInterval], sequences: pysam.FastaFile, extract_by_id: Optional[str] = None) -> Generator[str, None, Set[str]]:
     missing_chromosomes = set()
     for region in regions:
-        if str(region.chromosome) not in sequences.references:
-            missing_chromosomes.add(str(region.chromosome))
+        identifier = format_region(extract_by_id, region) if extract_by_id else str(region.chromosome)
+
+        if identifier not in sequences.references:
+            missing_chromosomes.add(identifier)
             continue
-        sequence = sequences.fetch(str(region.chromosome), region.start.position, region.stop.position)
+
+        sequence = sequences.fetch(identifier) if extract_by_id else \
+            sequences.fetch(identifier, region.start.position, region.stop.position)
         yield reverse_complement(sequence) if region.strand == '-' else sequence
+    print('\n'.join(sorted(missing_chromosomes)))
     return missing_chromosomes
+
+
+def format_region(format_string: str, region: GenomicInterval) -> str:
+    return format_string.format(chromosome=region.chromosome,
+                                start=region.start.position,
+                                end=region.stop.position,
+                                strand=region.strand,
+                                **region.data)
 
 
 def main():
@@ -30,13 +43,18 @@ def get_parser():
 
 
 def define_parser(parser):
-    add_arg = parser.add_argument
     parser.add_argument('input', nargs='?',
                         help='regions to extract (default: stdin).')
     parser.add_argument('output', nargs='?',
                         help='sequence file to extract sequences to (default: stdout).')
-    add_arg('-s', '--sequence', required=True,
-            help='sequence file to extract regions from')
+    parser.add_argument('-b', '--by',
+                        help='if defined, use this format string to query the sequences.')
+    parser.add_argument('-f', '--format', default='{gene_id}',
+                        help='format string to use as the header of the fasta entry.')
+    parser.add_argument('-i', '--input-format',
+                        help='file format of input file (useful for reading from stdin).')
+    parser.add_argument('-s', '--sequence', required=True,
+                        help='sequence file to extract regions from')
     parser.set_defaults(func=init_extract)
     return parser
 
@@ -45,7 +63,7 @@ def init_extract(args):
     wrapper = TextWrapper()
     with open_loci_file(args.input) as regions, open_file(args.output, 'w') as output:
         sequences = pysam.FastaFile(args.sequence)
-        for region, sequence in zip(regions, extract(regions, sequences)):
+        for region, sequence in zip(regions, extract(regions, sequences, args.by)):
             output.write('>{}\n{}\n'.format(region.data['gene_id'], '\n'.join(wrapper.wrap(sequence))))
 
 
