d2a24728716827877c476405864f63492f139261
Liam Childs <liam.childs@pei.de>
2020-03-22 14:55:30 +0100

    add multiple inputs to sequence stat

--- lhc/binf/sequence/tools/stat.py
+++ lhc/binf/sequence/tools/stat.py
@@ -20,9 +20,9 @@ def get_parser():
 
 
 def define_parser(parser):
-    parser.add_argument('input', nargs='?',
+    parser.add_argument('input', nargs='*',
                         help='sequences to filter (default: stdin).')
-    parser.add_argument('output', nargs='?',
+    parser.add_argument('-o', '--output',
                         help='sequence file to filtered sequences to (default: stdout).')
     parser.set_defaults(func=init_stat)
     return parser
@@ -30,9 +30,10 @@ def define_parser(parser):
 
 def init_stat(args: argparse.Namespace):
     with open_file(args.output, 'w') as output:
-        sequences = iter_fasta(args.input)
-        for sequence in sequences:
-            output.write('{}\t{}\n'.format(sequence.key, len(sequence.seq)))
+        for input in args.input:
+            sequences = iter_fasta(input)
+            for sequence in sequences:
+                output.write('{}\t{}\n'.format(sequence.key, len(sequence.seq)))
 
 
 if __name__ == '__main__':
