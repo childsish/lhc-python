853d1cbea26ab3be36a8f8d278c0ac1718c961b7
Liam Childs <liam.childs@pei.de>
2020-07-31 22:38:55 +0200

    improve bed column parsing

--- lhc/io/loci/bed.py
+++ lhc/io/loci/bed.py
@@ -11,10 +11,13 @@ class BedFile(LociFile):
         name = parts[3] if len(parts) > 3 else ''
         score = parts[4] if len(parts) > 4 else ''
         strand = parts[5] if len(parts) > 5 else '+'
+        data = {'gene_id': name, 'score': score}
+        for col in parts[6:]:
+            data[col] = None
         return GenomicInterval(int(parts[1]) - index, int(parts[2]),
                                chromosome=parts[0],
                                strand=strand,
-                               data={'gene_id': name, 'score': score, 'cols': parts[6:]})
+                               data=data)
 
     def format(self, interval: GenomicInterval, index=1) -> str:
         return '{chr}\t{start}\t{stop}\t{data[gene_id]}\t{score}\t{strand}{cols}'.format(
