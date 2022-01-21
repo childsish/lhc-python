e79402c6f91a38998eed3eb95c3ab71c088d4ec4
Liam Childs <liam.h.childs@gmail.com>
2022-01-21 20:50:53 +0100

    refactor interval binner interface

--- lhc/interval/binner.py
+++ lhc/interval/binner.py
@@ -1,25 +1,25 @@
 from typing import Sequence, Tuple
 
 
-class IntervalBinner(object):
+class IntervalBinner:
     def __init__(self, minbin=2, maxbin=7):
-        self.minbin = minbin
-        self.maxbin = maxbin
+        self._minbin = minbin
+        self._maxbin = maxbin
     
     def get_bin(self, ivl):
-        for i in range(self.minbin, self.maxbin + 1):
+        for i in range(self._minbin, self._maxbin + 1):
             bin_level = 10 ** i
             if int(ivl.start / bin_level) == int(ivl.stop / bin_level):
-                return int(i * 10 ** (self.maxbin + 1) + int(ivl.start / bin_level))
-        return int((self.maxbin + 1) * 10 ** (self.maxbin + 1))
+                return int(i * 10 ** (self._maxbin + 1) + int(ivl.start / bin_level))
+        return int((self._maxbin + 1) * 10 ** (self._maxbin + 1))
 
     def get_overlapping_bins(self, ivl) -> Sequence[Tuple[int, int]]:
         res = []
-        big_bin = int((self.maxbin + 1) * 10 ** (self.maxbin + 1))
-        for i in range(self.minbin, self.maxbin + 1):
+        big_bin = int((self._maxbin + 1) * 10 ** (self._maxbin + 1))
+        for i in range(self._minbin, self._maxbin + 1):
             bin_level = 10 ** i
-            fr = int(i * 10 ** (self.maxbin + 1) + int(ivl.start / bin_level))
-            to = int(i * 10 ** (self.maxbin + 1) + int(ivl.stop / bin_level))
+            fr = int(i * 10 ** (self._maxbin + 1) + int(ivl.start / bin_level))
+            to = int(i * 10 ** (self._maxbin + 1) + int(ivl.stop / bin_level))
             res.append((fr, to + 1))
         res.append((big_bin, big_bin + 1))
         return res
