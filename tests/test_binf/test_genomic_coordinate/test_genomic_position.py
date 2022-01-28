033749479376d65eef93164c22343124b8421e7d
Liam Childs <liam.h.childs@gmail.com>
2022-01-28 18:49:33 +0100

    fix test genomic position

--- tests/test_binf/test_genomic_coordinate/test_genomic_position.py
+++ tests/test_binf/test_genomic_coordinate/test_genomic_position.py
@@ -5,30 +5,30 @@ from lhc.binf.genomic_coordinate import GenomicPosition
 
 class TestGenomicPosition(unittest.TestCase):
     def test_eq(self):
-        a = GenomicPosition('1', 0)
-        b = GenomicPosition('1', 0)
-        c = GenomicPosition('1', 1)
-        d = GenomicPosition('2', 0)
+        a = GenomicPosition(0, chromosome='1')
+        b = GenomicPosition(0, chromosome='1')
+        c = GenomicPosition(1, chromosome='1')
+        d = GenomicPosition(0, chromosome='2')
 
         self.assertEqual(a, b)
         self.assertNotEqual(a, c)
         self.assertNotEqual(a, d)
 
     def test_lt(self):
-        a = GenomicPosition('1', 0)
-        b = GenomicPosition('1', 0)
-        c = GenomicPosition('1', 1)
-        d = GenomicPosition('2', 0)
+        a = GenomicPosition(0, chromosome='1')
+        b = GenomicPosition(0, chromosome='1')
+        c = GenomicPosition(1, chromosome='1')
+        d = GenomicPosition(0, chromosome='2')
 
         self.assertFalse(a < b)
         self.assertLess(a, c)
         self.assertLess(a, d)
 
     def test_get_distance(self):
-        a = GenomicPosition('1', 0)
-        b = GenomicPosition('1', 0)
-        c = GenomicPosition('1', 1)
-        d = GenomicPosition('2', 0)
+        a = GenomicPosition(0, chromosome='1')
+        b = GenomicPosition(0, chromosome='1')
+        c = GenomicPosition(1, chromosome='1')
+        d = GenomicPosition(0, chromosome='2')
 
         self.assertEqual(0, b.get_distance_to(a))
         self.assertEqual(1, c.get_distance_to(a))
