ab04dfa5ec525e57d116dd5d54e093c108a2d175
Liam Childs <liam.h.childs@gmail.com>
2022-02-18 18:22:37 +0100

    remove multidimensionmap

--- lhc/collections/__init__.py
+++ lhc/collections/__init__.py
@@ -2,7 +2,6 @@ from .inorder_access_set import InOrderAccessSet
 from .inorder_access_interval_set import InOrderAccessIntervalSet
 from .interval_map import IntervalMap
 from .interval_set import IntervalSet
-from .multi_dimension_map import MultiDimensionMap
 from .sequence_set import SequenceSet
 from .tracked_set import TrackedSet
 from .tracked_map import TrackedMap
