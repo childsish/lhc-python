bba34c3bf0a4b13e1feefa576bdde9fc563da44d
Liam Childs <liam.childs@pei.de>
2022-02-27 07:32:01 +0100

    fix alignment fetch

--- lhc/io/alignment/alignment.py
+++ lhc/io/alignment/alignment.py
@@ -4,4 +4,13 @@ class Alignment:
 
     def fetch(self, reference, start, stop):
         reference_sequence = self.sequences[reference]
-        return {key: value[start:stop] for key, value in self.sequences.items()}
+        reference_position = 0
+        alignment_start = 0
+        alignment_stop = 0
+        for alignment_position in range(len(reference_sequence)):
+            reference_position += reference_sequence[alignment_position] != '-'
+            if reference_position == start:
+                alignment_start = alignment_position + 1
+            if reference_position == stop:
+                alignment_stop = alignment_position + 1
+        return {key: value[alignment_start:alignment_stop] for key, value in self.sequences.items()}
