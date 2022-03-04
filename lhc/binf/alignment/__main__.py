49b5559ef658d703ec3cd2566aeaee6615625eda
Liam Childs <liam.h.childs@gmail.com>
2022-03-04 19:01:09 +0100

    convert all tools to use main_with_tools

--- lhc/binf/alignment/__main__.py
+++ lhc/binf/alignment/__main__.py
@@ -1,42 +1,11 @@
-import argparse
+import os
 
-from .tools import call_variants, extract, get_consensus, strand, mismatch_filter, trim_gaps
-
-
-def main():
-    args = get_parser().parse_args()
-    args.func(args)
-
-
-def get_parser():
-    return define_parser(argparse.ArgumentParser())
-
-
-def define_parser(parser):
-    parser.set_defaults(func=lambda args: parser.print_usage())
-    subparsers = parser.add_subparsers()
-
-    call_variants_parser = subparsers.add_parser('call_variants')
-    call_variants.define_parser(call_variants_parser)
-
-    extract_parser = subparsers.add_parser('extract')
-    extract.define_parser(extract_parser)
-
-    get_consensus_parser = subparsers.add_parser('get_consensus')
-    get_consensus.define_parser(get_consensus_parser)
-
-    strand_parser = subparsers.add_parser('strand')
-    strand.define_parser(strand_parser)
-
-    mismatch_filter_parser = subparsers.add_parser('mismatch_filter')
-    mismatch_filter.define_parser(mismatch_filter_parser)
-
-    trim_parser = subparsers.add_parser('trim_gaps')
-    trim_gaps.define_parser(trim_parser)
-
-    return parser
+from lhc.misc.argparse import main_with_tools
 
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main())
+    sys.exit(main_with_tools(
+        os.path.join(os.path.dirname(__file__), 'tools'),
+        'lhc.binf.alignment.tools',
+    ))
