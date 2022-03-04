a5af1000cc1ffd37fd174abb4b8ede8887b75875
Liam Childs <liam.h.childs@gmail.com>
2022-03-04 18:42:07 +0100

    add misc.argparse

---
+++ lhc/misc/argparse.py
@@ -0,0 +1,29 @@
+import argparse
+import importlib
+import os
+import sys
+
+
+def main_with_tools(tools_dir, package_name):
+    parser = define_parser(argparse.ArgumentParser(), tools_dir, package_name)
+    args = parser.parse_args()
+    args.func(args)
+
+
+def define_parser(parser, tools_dir, package_name):
+    parser.set_defaults(func=lambda args: parser.print_usage())
+    subparsers = parser.add_subparsers()
+    for module_file in os.listdir(tools_dir):
+        module_name = module_file.rsplit(".", 1)[0]
+        if not module_file.endswith('.py') or module_file.startswith('__'):
+            continue
+        try:
+            module = importlib.import_module(f'{package_name}.{module_name}')
+            if hasattr(module, 'define_parser'):
+                subparser = subparsers.add_parser(module_name)
+                module.define_parser(subparser)
+            else:
+                sys.stderr.write(f'warning: module "{module_name}" does not define `define_parser` function\n')
+        except ModuleNotFoundError as e:
+            sys.stderr.write(f'warning: unable to import module "{module_name}": {e}\n')
+    return parser
