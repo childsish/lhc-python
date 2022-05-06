00099e82ddd6ddc0ef775fd89c824b9f99eaf7ae
Liam Childs <liam.h.childs@gmail.com>
2022-05-06 17:28:29 +0200

    add OnePassIntervalSet

--- lhc/collections/__init__.py
+++ lhc/collections/__init__.py
@@ -1,4 +1,2 @@
-from .inorder_access_set import InOrderAccessSet
-from .interval_map import IntervalMap
-from .interval_set import IntervalSet
-from .sequence_set import SequenceSet
+from lhc.collections.one_pass_interval_set import OnePassIntervalSet
+from lhc.collections.sequence_set import SequenceSet
---
+++ lhc/entities/__init__.py
@@ -0,0 +1,2 @@
+from lhc.entities.interval import Interval
+from lhc.entities.variant import Variant
