c33c6ea7a3d675be3b9d8723278c5a8d40587bd4
Liam H. Childs <liam.childs@pei.de>
2024-05-06 11:37:08 +0200

    convert to src layout from flat layout

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_namespace_packages, Extension
 
 bitap_extension = Extension(
     'lhc.misc.bitap',
-    ['lib/bitap/bitapmodule.cpp', 'lib/bitap/bitap.cpp'],
-    include_dirs=['./lib/bitap'])
+    ['src/lib/bitap/bitapmodule.cpp', 'src/lib/bitap/bitap.cpp'],
+    include_dirs=['src/lib/bitap'])
 
 digen_extension = Extension(
     'lhc.misc.digen',
-    ['lib/digen/digenmodule.cpp', 'lib/digen/digen.cpp'],
-    include_dirs=['./lib/digen'])
+    ['src/lib/digen/digenmodule.cpp', 'src/lib/digen/digen.cpp'],
+    include_dirs=['src/lib/digen'])
 
 setup(
     ext_modules=[digen_extension, bitap_extension],
